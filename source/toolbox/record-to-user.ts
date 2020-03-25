d0b8357e27fab17a30f0460e80106bc0cd7774ad
Chase Moskal <chasemoskal@gmail.com>
2020-03-25 04:06:32 -0700

    cleanup: remove moved stuff

--- source/toolbox/record-to-user.ts
+++
@@ -1,8 +0,0 @@
-
-import {UserRecord} from "../interfaces.js"
-import {User} from "authoritarian/dist/interfaces.js"
-
-export const recordToUser = ({_id, claims}: UserRecord): User => ({
-	claims,
-	userId: _id.toHexString(),
-})
